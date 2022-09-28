39da98e67a568f4c79a9a519a9c29cbfbe5bdba2
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-28 10:16:19 -0400

    made student class work and created freshman class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.csc325_oop_designreview_lab;
 
-/**
- *
- * @author MoaathAlrajab
- */
 public class MainClass {
 	
 	public static void main(String[] args) {
